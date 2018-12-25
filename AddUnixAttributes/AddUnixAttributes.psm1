fbde9021056505399a1fece50ad9dbc0dafdd1c3
Assen Totin <assen.totin@gmail.com>
2018-12-25 14:28:57 +0200

    Initial import

---
+++ AddUnixAttributes/AddUnixAttributes.psm1
@@ -0,0 +1,81 @@
+#Requires -Modules ActiveDirectory
+
+Import-Module ActiveDirectory
+
+function Add-UnixAttributes {
+	param (
+		[Parameter(Mandatory=$true)][string]$sAMAccountName,
+		[Parameter(Mandatory=$false)][string]$NISDomain,
+		[Parameter(Mandatory=$false)][string]$GIDNumber,
+		[Parameter(Mandatory=$false)][string]$LoginShell,
+		[Parameter(Mandatory=$false)][string]$UnixHomeDirectory
+	)
+
+	# Check our permissions - we need Admin
+	$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+	if (! $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+		Write-Host "ERROR: I need Administrator privilege to run. Run your PowerShell as Administrator."
+		Exit 1
+	}
+
+	# Fallback for NISDomain
+	if (! $NISDomain) {
+		$NISDomain = "int"
+	}
+
+	# Fallback for primary group
+	if (! $GIDNumber) {
+		$GIDNumber = "750"
+	}
+
+	# Fallback for shell
+	if (! $LoginShell) {
+		$LoginShell = "/bin/bash"
+	}
+
+	# Fallback for home
+	if (! $UnixHomeDirectory) {
+		$UnixHomeDirectory = "/home/$sAMAccountName"
+	}
+
+	# Get user properties
+	try {
+		$AccountUIDNumber = (Get-ADUser -Identity $sAMAccountName -Properties uidNumber).uidNumber
+	}
+	catch {
+		Write-Host "ERROR: Unable to find user $sAMAccountName"
+		Exit 1
+	}
+
+	# Check if we already have a UID
+	if ($AccountUIDNumber ) {
+		Write-Host "ERROR: User $sAMAccountName already has UID $AccountUIDNumber"
+		Exit 1
+	}
+
+	# Figure out all UIDs that were already assigned
+	$AllUsersProperties = Get-ADUser -Filter ‘uidNumber -like "*"’ -Properties uidNumber
+	$MaxUIDNumber = 0
+	Foreach ($UserUIDNumber in $AllUsersProperties.uidNumber) {
+		if ($MaxUIDNumber -lt $UserUIDNumber) {
+			$MaxUIDNumber = $UserUIDNumber
+		}
+	}
+
+	# Choose new UID as max UID plus 1
+	$UIDNumber = $MaxUIDNumber + 1
+
+	# Update the user wuth Unix attributes
+	try {
+		Set-ADUser -Identity $sAMAccountName -Add @{msSFU30Name="$sAMAccountName"; uid="$sAMAccountName"; msSFU30NisDomain="$NISDomain"; uidNumber="$UIDNumber"; gidNumber="$GIDNumber"; loginShell="$LoginShell"; UnixHomeDirectory="$UnixHomeDirectory" } 
+	}
+	catch {
+		Write-Host "ERROR: Unable to set attributes to user $sAMAccountName"
+		Exit 1
+	}
+
+	Write-Host "User with sAMAccountName $sAMAccountName updated (UID set to $UIDNumber)"
+}
+
+Export-ModuleMember -Function Add-UnixAttributes
+
--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # powershell-unixattributes
-CmdLet to add Unix Attributes to AD user
+
+PowerShell cmdlet for adding Unix Attributes to AD user
+
+This is a small cmdlet to add Unix Attributes to an existing Windows Active Directory. It is especially useful on Windows Server 2016 where Microsoft removed the NIS service and the GUI tools for managing Unix Attributes from ADUC.
+
+# Usage
+
+    Add-UnixAttributes -sAMAccountName [-NISDomain] [-GIDNumber] [-LoginShell] [-UnixHomeDirectory]
+
+Parameters:
+
+* `sAMAccountName`: the username to add Unix Attributes to.
+
+* `NISDomain`: the NIS Domain to use (default: `int`)
+
+* `GIDNumber`: the primary GID to assign (default: `750`)
+
+* `LoginShell`: the login shell to assign (default: `/bin/bash`)
+
+* `UnixHomeDirectory`: the home directory to set (default: `/home/<sAMAccountName>`)
+
+# Build
+
+The script is already written in PowerShell, so you do not have to build it. 
+
+## Usage
+
+To import the script in your current session: `Import-Module Add-UnixAttributes.ps1`. Alternarively, run it from its directory.
+
+You must use an Administrator shell for it to work. 
+
+You need to have PowerShell ActiveDirectory module installed (part of RSAT).
+
+## Package
+
+You can use the provided NSIS script to build a GUI installer with the script. NSIS is available for Windows and Linux, see http://nsis.sourceforge.net (on Linux, be sure to check with your distro first):
+
+* From the top level directoy of the repo, call `makensis Add-UnixAttributes.nsis`.
+
+# Binary Downloads
+
+See home page for compiled binary downloads: http://www.zavedil.com/powershell-cmdlet-for-unix-attributes
+
+# Bugs & Wishes
+
+Report bugs & wishes here or contact the author via email.
+
+# Author
+
+Assen Totin, assen.totin@gmail.com
+
